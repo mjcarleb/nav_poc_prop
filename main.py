a91ad9335c798b6f321c04c5bb07f20137ff28ae
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-07-26 09:23:23 -0400

    starting

--- main.py
+++ main.py
@@ -6,40 +6,60 @@ import matplotlib.pyplot as plt
 np.random.seed(1)
 
 #############################################
-#    HISTORICAL MARKET PRICES
+#    HISTORICAL MARKET PRICE CHANGES
 #############################################
 market_changes = np.random.normal(loc=.07/220, scale=.01, size=90)
-#plt.plot(market_changes)
-#plt.show()
 print(f"Mean of daily price change = {np.mean(market_changes)}")
 print(f"220 * mean = {220 *np.mean(market_changes)}")
+
 
 #############################################
 #    INDEX PROFILES
 #############################################
 index_names = [f"idx{n}" for n in range(11,34)]
-index_profiles = dict()
+index_betas = dict()
 for name in index_names:
 
-    start_price = np.random.uniform(low=10, high=110)
+    #start_price = np.random.uniform(low=10, high=110)
     beta = np.random.uniform(low=-1, high=1)
-    index_profiles[name] = [start_price, beta]
+    index_betas[name] = beta
+
 
 #############################################
-#    HISTORICAL INDEX PRICES
+#    HISTORICAL INDEX PRICE CHANGES
 #############################################
-
-index_prices = dict()
+index_changes = dict()
 for name in index_names:
 
-    start_price = index_profiles[name][0]
-    beta = index_profiles[name][1]
-    index_prices[name] = start_price + start_price * market_changes * beta
+    beta = index_betas[name]
+    index_changes[name] = market_changes * beta
+
+
+#############################################
+#    FUND PROFILES
+#############################################
+fund_names = ["Fund1", "Fund2", "Fund3"]
+fund_profiles = dict()
+fund_profiles['Fund1'] = (32.61, ["idx11", "idx12", "idx13"])
+fund_profiles['Fund2'] = (17.08, ["idx21", "idx22", "idx23"])
+fund_profiles['Fund3'] = (98.39, ["idx31", "idx32", "idx33"])
+
+
+#############################################
+#    HISTORICAL FUND PRICES
+#############################################
+fund_prices = dict()
+for name in fund_names:
+
+    start_price = fund_profiles[name][0]
+    best_index = fund_profiles[name][1][0]
+    fund_prices[name] = start_price + start_price * market_changes * beta
+
+
+
+
 
 # Create 5 funds with list of indices
-funds = dict()
-funds['Fund1'] = ["idx11", "idx12", "idx13"]
-funds['Fund2'] = ["idx21", "idx22", "idx23"]
-funds['Fund3'] = ["idx31", "idx32", "idx33"]
+
 
 
